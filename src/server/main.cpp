dfe44830f0d6b5eca52568be5b1e34ef211cf1a3
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2017-11-13 13:34:13 +0000

    WIP TCP Client Connection Code for time sync
    	- Abruptly stopping as need to go to meeting

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -6,12 +6,48 @@
 ////////////////////////////////////////////////////////////////////////////
 
 #include <cstdio>
+#include <thread>
+#include <vector>
 
 #include <SFML/Network.hpp>
+#include <SFML/System/Time.hpp>
 
 #define NET_PORT 1337 // Port that we (the server) should run on
 
-int main(int argc, const char** argv){
+void thread_timeSyncer(sf::TcpSocket *client) {
+
+}
+
+void thread_timeSyncListener() {
+	sf::TcpListener listener;
+
+	if (listener.listen(NET_PORT) != sf::Socket::Done) {
+		// error ..
+	}
+
+	std::vector<sf::TcpSocket*> clientSockets;
+	std::vector<std::thread*> clientThreads;
+	while (true) { //TODO Maybe update this to have some sort of stop case?
+		// Make a new client
+		sf::TcpSocket *client = new sf::TcpSocket;
+
+		// Listen for the client, when one connects add to List of
+		// clients and deal with time sync protocol
+		if (listener.accept(*client) == sf::Socket::Done) {
+			clientSockets.push_back(client);
+			std::thread *t = new std::thread(thread_timeSyncer, client);
+		} else {
+			// Error, cleanup the client we created
+			delete client;
+		}
+		
+	}
+
+	listener.close();
+}
+
+
+int main(int argc, const char** argv) {
 	/////////////////////////////////////////////////
 	// Setup Networking
 	sf::UdpSocket socket;
