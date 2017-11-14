6bba06b32356e822c356e6782002e253bb992094
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2017-11-14 17:51:21 +0000

    Added placeholder Game State to model.hpp

--- src/lib-tempo/include/tempo/model.hpp
+++ src/lib-tempo/include/tempo/model.hpp
@@ -10,6 +10,12 @@
 #ifndef TEMPO_SONG_HPP
 #define TEMPO_SONG_HPP
 
+#include <cstdint>
+#include <limits>
+
+#define MP_ID_TYPE int16_t
+#define MP_MAX_PLAYERS std::numeric_limits<MP_ID_TYPE>::max()
+
 #undef main // SDL likes to define main
 
 namespace tempo
@@ -17,9 +23,14 @@ namespace tempo
 	// Temporary player struct - will be upgraded or replaced with class,
 	// just for the purpose of writing server code
 	typedef struct {
-		int32_t mp_id;
-		int16_t x, y;
+		MP_ID_TYPE mp_id; // Multiplayer ID
+		int16_t x, y;  // Position, x and y
 	} Player;
+
+	// Game State - same temporary disclaimer as Player
+	typedef struct {
+		Player players[MP_MAX_PLAYERS];
+	} GameState;
 }
 
 #endif
