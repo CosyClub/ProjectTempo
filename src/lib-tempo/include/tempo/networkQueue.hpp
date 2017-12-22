d44cc083c34af12c4d83e2999577621b7a3e9a4c
Raef Coles <RaefColes@gmail.com>
2017-12-22 15:37:40 +0000

    Slight refactor of networkQueue

--- src/lib-tempo/include/tempo/networkQueue.hpp
+++ src/lib-tempo/include/tempo/networkQueue.hpp
@@ -15,21 +15,23 @@
 
 namespace tempo
 {
-	////////////////////////////////////////////////////////////////////////
-	/// Function declarations
 
-	// get_system_queue
-	// Gets a pointer to the network interface queue for a particular system
-	//
-	// Usage:
-	// 	tempo::Queue<Packet>* q = get_system_queue(tempo::SystemQID id)
-	//
-	// Arguments:
-	// 	id	- The systemQID of the system
-	//
-	// Returns:
-	// 	Pointer to a tempo::Queue<sf::Packet>
-	tempo::Queue<sf::Packet>* get_system_queue(tempo::SystemQID);
+////////////////////////////////////////////////////////////////////////
+/// Function declarations
+
+// get_system_queue
+// Gets a pointer to the network interface queue for a particular system
+//
+// Usage:
+// 	tempo::Queue<Packet>* q = get_system_queue(tempo::SystemQID id)
+//
+// Arguments:
+// 	id	- The systemQID of the system
+//
+// Returns:
+// 	Pointer to a tempo::Queue<sf::Packet>
+tempo::Queue<sf::Packet>* get_system_queue(tempo::SystemQID);
+
 }
 
 #endif
--- src/lib-tempo/src/networkQueue.cpp
+++ src/lib-tempo/src/networkQueue.cpp
@@ -7,11 +7,13 @@
 
 namespace tempo
 {
-	//Hidden queue list
-	tempo::Queue<sf::Packet> system_queues[tempo::QID_RESERVED_TOP];
 
-	tempo::Queue<sf::Packet>* get_system_queue(tempo::SystemQID id)
-	{
-		return &(system_queues[id]);
-	}
+//Hidden queue list
+tempo::Queue<sf::Packet> system_queues[tempo::QID_RESERVED_TOP];
+
+tempo::Queue<sf::Packet>* get_system_queue(tempo::SystemQID id)
+{
+	return &(system_queues[id]);
+}
+
 }
