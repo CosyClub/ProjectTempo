95e5d26ad4405ce817f59de7edd611fba9f246a3
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-01-22 20:45:08 +0000

    Add: Slightly thinned health bars

--- src/lib-tempo/src/entity/Render.cpp
+++ src/lib-tempo/src/entity/Render.cpp
@@ -27,7 +27,7 @@ namespace tempo{
 		// Create Billboard using Ogre Billboards
 		Ogre::BillboardSet* Healthset = this->scene->createBillboardSet();
 		Healthset->setMaterialName("rectangleSprite");
-		Healthset->setDefaultDimensions(.5, .5);
+		Healthset->setDefaultDimensions(0.5, 0.2);
 		Healthset->setBillboardType(Ogre::BBT_ORIENTED_COMMON);
 		Healthset->setCommonDirection(Ogre::Vector3(0, 1, 0));
 		Ogre::Billboard* health = Healthset->createBillboard(0.5, 2, 0);
