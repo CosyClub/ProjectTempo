b62fe121f458f2c92f47f09761c91a4b21777084
Jamie Terry <jnterry@ntlworld.com>
2018-02-12 21:48:25 +0000

    Add simple light test

--- src/irrlicht-test/main.cpp
+++ src/irrlicht-test/main.cpp
@@ -6,6 +6,7 @@
 
 #include <cstdio>
 #include <irrlicht.h>
+#include <SFML/System/Clock.hpp>
 
 int main(const int argc, const char** argv){
 	irr::IrrlichtDevice* device = irr::createDevice(irr::video::EDT_OPENGL,
@@ -62,11 +63,40 @@ int main(const int argc, const char** argv){
 
 	smgr->addCameraSceneNode(0, irr::core::vector3df(0,30,-80), irr::core::vector3df(0,5,0));
 
+
+
+	irr::scene::ILightSceneNode* light_0_node = smgr->addLightSceneNode(0,
+	                                                                    irr::core::vector3df(0,0,0),
+	                                                                    irr::video::SColorf(1.0f, 1.0f, 1.0f),
+	                                                                    1000.0f);
+	irr::video::SLight& light_0 = light_0_node->getLightData();
+	light_0_node->enableCastShadow();
+	light_0.Attenuation = irr::core::vector3df(0, 0.0001, 0.00001);
+
+	{
+		auto* node = smgr->addBillboardSceneNode(light_0_node, irr::core::dimension2d<irr::f32>(10, 10));
+		node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+		node->setMaterialType(irr::video::EMT_TRANSPARENT_ADD_COLOR);
+		node->setMaterialTexture(0, driver->getTexture("resources/materials/textures/particlewhite.bmp"));
+	}
+
+
+	irr::core::matrix4 light_transform;
+
+	sf::Clock game_timer;
+
 	printf("Entering main loop\n");
 	while(device->run()){
+		float game_time = game_timer.getElapsedTime().asSeconds();
 		driver->beginScene(true, true, irr::video::SColor(255, 30, 30, 30));
 
-		node_bunny->setRotation(irr::core::vector3df(0, 90, 0));
+		node_bunny->setRotation(irr::core::vector3df(0, game_time * 60, 0));
+
+		light_transform.setRotationDegrees(irr::core::vector3df(0, game_time * -70, 0));
+
+		irr::core::vector3df light_0_position(30, 0, 0);
+		light_transform.transformVect(light_0_position);
+		light_0_node->setPosition(light_0_position);
 
 		smgr->drawAll();
 		guienv->drawAll();
