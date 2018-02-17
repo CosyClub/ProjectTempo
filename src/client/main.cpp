4b0aba328aba6224332f9b8f292f46783dcef1ce
Jamie Terry <jnterry@ntlworld.com>
2018-02-17 22:27:55 +0000

    Add camera and dummy mesh

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -20,13 +20,29 @@ int main(int argc, const char** argv){
 
 	smgr->setAmbientLight(irr::video::SColorf(0.5f, 0.5f, 0.5f, 0.5f));
 
+	smgr->addCameraSceneNode(nullptr, // parent node
+	                         irr::core::vector3df(0,30,80), // position
+	                         irr::core::vector3df(0, 0, 0)  // look at
+	                        );
+
+	irr::scene::IAnimatedMesh* mesh_hills = smgr->addHillPlaneMesh(
+	                                                               L"test_hill_mesh",
+		irr::core::dimension2d<irr::f32>(  1,   1), // tile size
+		irr::core::dimension2d<irr::u32>(100, 100), // tile count
+		nullptr, // material
+		1.0f // hill height
+	);
+	irr::scene::IAnimatedMeshSceneNode* node_hills = smgr->addAnimatedMeshSceneNode(mesh_hills);
+	if (node_hills){
+		node_hills->setPosition(irr::core::vector3df(0, 0, 0));
+	}
+
 	printf("Entering main loop\n");
 	while(device->run()){
 		smgr->drawAll();
 		gui_env->drawAll();
 
 		driver->endScene();
-
 	}
 	printf("Left main loop\n");
 
