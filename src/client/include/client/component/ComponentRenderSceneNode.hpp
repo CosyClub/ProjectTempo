f762e468efce83f1d473a0cac84fdd9054a6e467
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2018-02-18 22:11:10 +0000

    All players get billboards

--- src/client/include/client/component/ComponentRenderSceneNode.hpp
+++ src/client/include/client/component/ComponentRenderSceneNode.hpp
@@ -8,7 +8,6 @@
 namespace client {
 	struct ComponentRenderSceneNode : public anax::Component {
 		irr::scene::ISceneNode* node;
-		
 		ComponentRenderSceneNode(irr::scene::ISceneNode* node);
 	};
 
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -5,6 +5,7 @@
 #include <anax/Entity.hpp>
 
 #include <client/system/SystemStageRenderer.hpp>
+#include <client/system/SystemRenderSceneNode.hpp>
 #include <client/component/ComponentRenderSceneNode.hpp>
 
 #include <tempo/component/ComponentStagePosition.hpp>
@@ -21,6 +22,7 @@ void createEntityStage(anax::World& world){
 void createEntityPlayer(anax::World& world) {
 	printf("Creating entity player\n");
 	anax::Entity entity_player = world.createEntity();
+	entity_player.addComponent<tempo::ComponentStage>("resources/levels/levelTest.bmp");
 	entity_player.addComponent<tempo::ComponentStagePosition>(glm::ivec2(0, 0));
 	entity_player.addComponent<client::ComponentRenderSceneNode>(nullptr);
 	entity_player.activate();
@@ -43,25 +45,31 @@ int main(int argc, const char** argv){
 	irr::scene::ISceneManager* smgr    = device->getSceneManager();
 	irr::gui::IGUIEnvironment* gui_env = device->getGUIEnvironment();
 
+	// Debug
 	smgr->setAmbientLight(irr::video::SColorf(0.1f, 0.1f, 0.1f));
 
+
+	irr::scene::ICameraSceneNode* camera_node;
 	if (false) {
 		float rotateSpeed = 25.0f;
 		float moveSpeed = 0.1f;
-		irr::scene::ICameraSceneNode* camera_node = smgr->addCameraSceneNodeFPS(nullptr, rotateSpeed, moveSpeed);
+		camera_node = smgr->addCameraSceneNodeFPS(nullptr, rotateSpeed, moveSpeed);
 		device->getCursorControl()->setVisible(false);
 	} else {
 		float rotate = -100.0f;
 		float translate = -100.0f;
 		float zoom = 100.0f;
 		float distance = 10.0f;
-		irr::scene::ICameraSceneNode* camera_node = smgr->addCameraSceneNodeMaya(nullptr, rotate, translate, zoom, -1, distance);
+		camera_node = smgr->addCameraSceneNodeMaya(nullptr, rotate, translate, zoom, -1, distance);
 		device->getCursorControl()->setVisible(true);
 		camera_node->setPosition(irr::core::vector3df(10.0f, 10.0f, 10.0f));
 		camera_node->setTarget(irr::core::vector3df(0.0f, 0.0f, 0.0f));
 	}
-
-	smgr->addLightSceneNode(nullptr, irr::core::vector3df(0.0f, 10.0f, 0.0f));
+	irr::scene::ISceneNode* light_node = smgr->addLightSceneNode(
+		camera_node,
+		irr::core::vector3df(0.0f, 10.0f, 0.0f),
+		irr::video::SColorf(1.0f, 1.0f, 1.0f),
+		1000.0f);
 
 	irr::core::dimension2d<irr::f32> size(1.0f, 2.0f);
 	irr::core::vector3df position(0.0f, 0.0f, 0.0f);
@@ -74,15 +82,19 @@ int main(int argc, const char** argv){
 		red,
 		red);
 
-	/////////////////////////////////////////////////
-	// Setup scene
+	// Create World
 	anax::World world;
 	client::SystemStageRenderer system_stage_renderer;
+	client::SystemRenderSceneNode system_render_scene_node;
 	world.addSystem(system_stage_renderer);
+	world.addSystem(system_render_scene_node);
 	createEntityStage(world);
-	
+	createEntityPlayer(world);
+
+	// Setup Systems
 	world.refresh();
 	system_stage_renderer.setup(smgr);
+	system_render_scene_node.setup(smgr);
 
 	printf("Entering main loop\n");
 	while(device->run()){
