461260a71129ae59ef5a25bc21adbc8ca37bfaf2
Raef Coles <RaefColes@gmail.com>
2018-02-20 15:50:35 +0000

    Add systemGraphicsCreation

--- src/lib-tempo/include/tempo/system/SystemGraphicsCreation.hpp
+++ src/lib-tempo/include/tempo/system/SystemGraphicsCreation.hpp
@@ -3,13 +3,19 @@
 
 #include <anax/System.hpp>
 
+#include <Ogre.h>
+
+#include <tempo/component/ComponentRender.hpp>
 #include <tempo/component/ComponentStagePosition.hpp>
+#include <tempo/component/ComponentModel.hpp>
 
 namespace tempo {
 
-	struct SystemGraphicsCreation : anax::System<anax::Requires<ComponentStagePosition>> {
-
-		void addEntities();
+	struct SystemGraphicsCreation : anax::System<anax::Requires<ComponentStagePosition,
+	                                                            ComponentModel>>
+	{
+		SystemGraphicsCreation();
+		void addEntities(Ogre::SceneManager* scene);
 
 	};
 }
--- src/lib-tempo/src/system/SystemGraphicsCreation.cpp
+++ src/lib-tempo/src/system/SystemGraphicsCreation.cpp
@@ -1,10 +1,26 @@
 #include <tempo/system/SystemGraphicsCreation.hpp>
 
+#include <iostream>
+
 namespace tempo {
 
-void SystemGraphicsCreation::addEntities() 
+SystemGraphicsCreation::SystemGraphicsCreation()
 {
+}
 
+void SystemGraphicsCreation::addEntities(Ogre::SceneManager* scene)
+{
+	auto entities = getEntities();
+	std::cout << "Adding graphics for " << entities.size() << " entities" << std::endl;
+	for (auto& entity : entities)
+	{
+		if (!entity.hasComponent<ComponentRender>())
+		{
+			std::cout << "Adding render component to entity with server ID " << entity.getId().index << std::endl;
+			std::string path = entity.getComponent<ComponentModel>().path;
+			entity.addComponent<ComponentRender>(scene, path);
+		}
+	}
 }
 
 }
