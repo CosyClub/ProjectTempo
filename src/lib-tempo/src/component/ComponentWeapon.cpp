8d7c37d36fb7e5926de987d10d88075db7175579
Raef Coles <RaefColes@gmail.com>
2018-02-22 21:59:27 +0000

    Simplify ComponentWeapon and add networking

--- src/lib-tempo/src/component/ComponentWeapon.cpp
+++ src/lib-tempo/src/component/ComponentWeapon.cpp
@@ -1,17 +1,34 @@
 #include <tempo/component/ComponentWeapon.hpp>
 
-
 namespace tempo
 {
-	ComponentWeapon::ComponentWeapon(int rate, Mask m)
-	{
-		attack_rate = rate;
-		last_attack = sf::microseconds(0);
-		damage = m;
-	}
 
-	float ComponentWeapon::GetDamage(glm::vec2 offset)
-	{
-		return damage.get_value(offset);
-	}
+ComponentWeapon::ComponentWeapon(Mask& m)
+{
+	damage = Mask(m);
+}
+
+float ComponentWeapon::GetDamage(glm::vec2 offset)
+{
+	return damage.get_value(offset);
+}
+
+ComponentWeapon::ComponentWeapon(sf::Packet p)
+{
+	p >> damage;
+}
+
+sf::Packet ComponentWeapon::dumpComponent()
+{
+	sf::Packet p;
+
+	p << damage;
+
+	return p;
+}
+ComponentID ComponentWeapon::getId()
+{
+	return ComponentID::WEAPON;
+}
+
 }
