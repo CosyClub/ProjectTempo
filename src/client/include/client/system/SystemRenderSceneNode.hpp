b7a35d1bb21f7ccfe9ecb2104e42961be6bf1887
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2018-03-05 22:00:35 +0000

    Y aligned billboard scene nodes
    (These don't change their childen)

--- src/client/include/client/system/SystemRenderSceneNode.hpp
+++ src/client/include/client/system/SystemRenderSceneNode.hpp
@@ -4,7 +4,6 @@
 #include <tempo/component/ComponentStagePosition.hpp>
 #include <tempo/component/ComponentStage.hpp>
 
-#include <client/misc/CAxialBillboardSceneNode.h>
 #include <client/component/ComponentRenderSceneNode.hpp>
 
 #include <anax/System.hpp>
--- src/client/src/system/SystemRenderSceneNode.cpp
+++ src/client/src/system/SystemRenderSceneNode.cpp
@@ -1,64 +1,54 @@
 #include <client/system/SystemRenderSceneNode.hpp>
 
 #include <IBillboardSceneNode.h>
-
 #include <iostream>
 
+#include "client\misc\YAlignedBillboardSceneNode.hpp"
+
 namespace client {
-	void SystemRenderSceneNode::setup(irr::scene::ISceneManager* smgr) {
-		std::cout << "SystemRenderSceneNode initializing" << std::endl;
+void SystemRenderSceneNode::setup(irr::scene::ISceneManager *smgr) {
+  std::cout << "SystemRenderSceneNode initializing" << std::endl;
 
-		irr::core::dimension2d<irr::f32> size(1.0f, 2.0f);
-		irr::core::vector3df pos(0.0f, 0.0f + size.Height / 2, 0.0f);
-		irr::video::SColor top_color(255, 255, 0, 0);
-		irr::video::SColor bottom_color(255, 255, 0, 0);
+  irr::core::dimension2d<irr::f32> size(1.0f, 2.0f);
+  irr::core::vector3df pos(0.0f, 0.0f + size.Height / 2, 0.0f);
+  irr::video::SColor top_color(255, 255, 0, 0);
+  irr::video::SColor bottom_color(255, 255, 0, 0);
 
-		auto entities = getEntities();
+  auto entities = getEntities();
 
-		for (auto entity : entities)
-		{
-			std::cout << "Adding billboard" << std::endl;
-			client::ComponentRenderSceneNode& sn = entity.getComponent<client::ComponentRenderSceneNode>();
+  for (auto entity : entities) {
+    std::cout << "Adding billboard" << std::endl;
+    client::ComponentRenderSceneNode &sn =
+        entity.getComponent<client::ComponentRenderSceneNode>();
 
-			// a billboard has its origin in the centre, this node is used for alignement
-			sn.node = smgr->addEmptySceneNode();
-			sn.node->setPosition(irr::core::vector3df(0.0f, 0.0f, 0.0f));
+    // a billboard has its origin in the centre, this node is used for
+    // alignement
+    sn.node = smgr->addEmptySceneNode();
+    sn.node->setPosition(irr::core::vector3df(0.0f, 0.0f, 0.0f));
 
-		// 	irr::scene::IBillboardSceneNode* billboard = smgr->addBillboardSceneNode(
-		// 		sn.node,
-		// 		size,
-		// 		pos, // fix alignment
-		// 		-1,
-		// 		top_color,
-		// 		bottom_color);
-		// }
+	irr::scene::IBillboardSceneNode *billboard = new irr::scene::YAlignedBillboardSceneNode(
+		sn.node, smgr, -1, pos, size, top_color, bottom_color);
+  }
 
-		irr::scene::CAxialBillboardSceneNode* billboard = new irr::scene::CAxialBillboardSceneNode(
-			sn.node,
-			smgr,
-			100,
-			pos,
-			irr::core::vector3df(-50.f, 20.f, 0.f),
-			20);
-		}
+  update();
+} // namespace client
 
-		update();
-	}
+void SystemRenderSceneNode::update() {
+  auto entities = getEntities();
 
-	void SystemRenderSceneNode::update() {
-		auto entities = getEntities();
+  for (auto entity : entities) {
+    tempo::ComponentStage &s = entity.getComponent<tempo::ComponentStage>();
+    tempo::ComponentStagePosition &sp =
+        entity.getComponent<tempo::ComponentStagePosition>();
+    client::ComponentRenderSceneNode &sn =
+        entity.getComponent<client::ComponentRenderSceneNode>();
 
-		for (auto entity : entities)
-		{
-			tempo::ComponentStage& s = entity.getComponent<tempo::ComponentStage>();
-			tempo::ComponentStagePosition& sp = entity.getComponent<tempo::ComponentStagePosition>();
-			client::ComponentRenderSceneNode& sn = entity.getComponent<client::ComponentRenderSceneNode>();
+    glm::ivec2 pos = sp.getOrigin();
 
-			glm::ivec2 pos = sp.getOrigin();
-
-			//std::cout << "Moved billboard to: " << pos.x << ", " << s.getHeight(pos) << ", " << pos.y << std::endl;
-			sn.node->setPosition(irr::core::vector3df(pos.x, s.getHeight(pos), pos.y));
-			// TODO
-		}
-	}
+    // std::cout << "Moved billboard to: " << pos.x << ", " << s.getHeight(pos)
+    // << ", " << pos.y << std::endl;
+    sn.node->setPosition(irr::core::vector3df(pos.x, s.getHeight(pos), pos.y));
+    // TODO
+  }
+}
 } // namespace client
