0e8090cc3358111ecd291900ed348aa527ff14c6
Codrin Popa <cp15316@my.bristol.ac.uk>
2018-03-20 12:44:07 +0000

    Added joystix font and some changes to the combo bar

--- src/client/src/system/SystemRenderGUI.cpp
+++ src/client/src/system/SystemRenderGUI.cpp
@@ -9,12 +9,12 @@ void SystemRenderGUI::update(irr::video::IVideoDriver * driver,
 {
 	const irr::core::dimension2du &screenSize = driver->getScreenSize();
 
-	irr::gui::IGUIFont *font = gui_env->getFont("resources/fonts/liberation_sans.xml");
+	irr::gui::IGUIFont *font = gui_env->getFont("resources/fonts/joystix/Joystix.xml");
 	if (font) {
-		irr::core::stringw str = L"Combo: ";
+		irr::core::stringw str = L"Combo:";
 		str += combo;
-		font->setKerningWidth(6);
-		font->setKerningHeight(6);
+		// font->setKerningWidth(6);
+		// font->setKerningHeight(6);
 		font->draw(
 		  str.c_str(),
 		  irr::core::rect<irr::s32>(10, 0.9 * screenSize.Height, 0.1 * screenSize.Width, 300),
@@ -26,16 +26,21 @@ void SystemRenderGUI::update(irr::video::IVideoDriver * driver,
 
 	float scale = clock.beat_progress_desc();
 
+  int bar_width_left    = screenSize.Width / 2 - (screenSize.Width * 0.22) * scale;
+  int bar_width_right   = screenSize.Width / 2 + (screenSize.Width * 0.22) * scale;
+  int bar_height_top    = 0.91 * screenSize.Height;
+  int bar_height_bottom = 0.95 * screenSize.Height;
+
 	if (scale >= 0.3) {
 		driver->draw2DRectangle(
 		  colour_white,
-		  irr::core::rect<irr::s32>(screenSize.Width / 2 - 200 * scale, 0.94 * screenSize.Height,
-		                            screenSize.Width / 2 + 200 * scale, 0.96 * screenSize.Height));
+		  irr::core::rect<irr::s32>(bar_width_left, bar_height_top,
+		                            bar_width_right, bar_height_bottom));
 	} else {
 		driver->draw2DRectangle(
 		  colour_blue,
-		  irr::core::rect<irr::s32>(screenSize.Width / 2 - 200 * scale, 0.94 * screenSize.Height,
-		                            screenSize.Width / 2 + 200 * scale, 0.96 * screenSize.Height));
+		  irr::core::rect<irr::s32>(bar_width_left, bar_height_top,
+		                            bar_width_right, bar_height_bottom));
 	}
 }
 }
