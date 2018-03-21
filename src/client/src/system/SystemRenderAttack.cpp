c4f5ae8a7bac859928735f8a06b288546dab696c
Jamie Terry <jnterry@ntlworld.com>
2018-03-21 23:38:49 +0000

    Improve apperance of attack visualization, remove debug prints

--- src/client/src/system/SystemRenderAttack.cpp
+++ src/client/src/system/SystemRenderAttack.cpp
@@ -14,8 +14,6 @@ SystemRenderAttack::SystemRenderAttack(irr::video::IVideoDriver * driver,
 void SystemRenderAttack::update()
 {
 
-	printf("Rendering attack intents\n");
-
 	auto& entities = this->getEntities();
 	for(auto entity : entities){
 		auto& attack = entity.getComponent<tempo::ComponentAttack>();
@@ -25,16 +23,9 @@ void SystemRenderAttack::update()
 		auto& rot   = entity.getComponent<tempo::ComponentStageRotation>();
 		auto& stage = entity.getComponent<tempo::ComponentStage        >();
 
-		printf("Found an attacking entity, center: %i, %i, size: {%i, %i}\n",
-		       attack.damage.ctr.x, attack.damage.ctr.y,
-		       attack.damage.positions.size(),
-		       attack.damage.mask.size());
-
 		auto targets = attack.getAbsolutePositions(pos.getOrigin(), rot.facing);
 
 		for(int i = 0; i < targets.size(); ++i){
-			printf("  %i, %i\n", targets[i].x, targets[i].y);
-
 			if(this->particle_systems.count(targets[i]) != 0){
 				// Particle system already exists on this tile, skip it
 				continue;
@@ -45,14 +36,14 @@ void SystemRenderAttack::update()
 			irr::scene::IParticleEmitter* emitter = psystem->createBoxEmitter
 				(
 				 irr::core::aabbox3d<irr::f32>(-0.5f, 0, -0.5f,   0.5f, 1.0f, 0.5f), // size of emitter
-				 irr::core::vector3df(0.0f, 0.005f, 0.0f), // direction
-				 80, 100,                                  // emit rate
-				 irr::video::SColor(0, 200, 200, 200),     // darkest color
-				 irr::video::SColor(0, 255, 255, 255),     // brightest color
-				 800, 1500,                                // min and max age
-				 3.0f,                                     // emit angle
-				 irr::core::dimension2df(0.05f, 0.05f),      // min size
-				 irr::core::dimension2df(0.2f, 0.2f)       // max size
+				 irr::core::vector3df(0.0f, 0.0001f, 0.0f), // direction
+				 10, 20,                                    // emit rate
+				 irr::video::SColor(0, 200, 200, 40),        // darkest color
+				 irr::video::SColor(0, 255, 255, 70),        // brightest color
+				 800, 1500,                                 // min and max age
+				 50.0f,                                     // emit angle
+				 irr::core::dimension2df(0.05f, 0.05f),     // min size
+				 irr::core::dimension2df(0.20f, 0.20f)        // max size
 				 );
 
 			psystem->setEmitter(emitter);
@@ -75,13 +66,10 @@ void SystemRenderAttack::update()
 		}
 	}
 
-	printf("\n\n\n");
-
 }
 
 void SystemRenderAttack::endBeat()
 {
-	printf("Removing all attack particle systems\n");
 	for(auto it = particle_systems.begin(); it != particle_systems.end(); ++it)
 	{
 		it->second->remove();
