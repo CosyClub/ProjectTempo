4f618e06fb5467a67fe44eb0c8c06546d8ee986e
Codrin Popa <cp15316@my.bristol.ac.uk>
2018-04-09 15:44:52 +0100

    Replaced random colours with random HSV colours between red and purple

--- src/client/src/system/SystemLighting.cpp
+++ src/client/src/system/SystemLighting.cpp
@@ -1,5 +1,5 @@
 #include <client/system/SystemLighting.hpp>
-
+#include <client/misc/RGBtoHSV.hpp>
 #include <IBillboardSceneNode.h>
 
 
@@ -27,7 +27,7 @@ namespace client {
 		this->light_ML1->setVisible(true);
 		this->data_ML1.Type = irr::video::ELT_POINT; // ELT_POINT | ELT_SPOT | ELT_DIRECTIONAL
 
-		this->data_ML1.AmbientColor = irr::video::SColorf(0, 0, 0, 0); 
+		this->data_ML1.AmbientColor = irr::video::SColorf(0, 0, 0, 0);
 		this->data_ML1.SpecularColor = irr::video::SColorf(0, 0, 0, 0);
 		this->data_ML1.DiffuseColor = irr::video::SColorf(0, 0, 0, 0);
 		this->light_ML1->setLightData(data_ML1);
@@ -36,13 +36,27 @@ namespace client {
 
 	void SystemLighting::update() {
 
+		irr::video::SColor colour_red(255, 255, 0, 0);
+		irr::video::SColor colour_purple(255, 255, 0, 255);
+
+		double scale = (double) ((1.0 - 0.0)*((double)rand() / RAND_MAX)) + 0.0; // from (0.0 to 1.0)
+
+		irr::core::vector3df c1 = RGBtoHSV(colour_red);
+		irr::core::vector3df c2 = RGBtoHSV(colour_purple);
+		c1.X = c1.X * scale + c2.X * (1.f - scale);
+		irr::video::SColor colour = HSVtoRGB(c1);
+		//healthbar->setColor(colour_health);
+
 		/// Animated light: ( Change to a random color... )
-		irr::f32 red = ((1.0 - 0.0)*((float)rand() / RAND_MAX)) + 0.0; // from (0.0 to 1.0)
-		irr::f32 green = ((1.0 - 0.0)*((float)rand() / RAND_MAX)) + 0.0; // from (0.0 to 1.0)
-		irr::f32 blue = ((1.0 - 0.0)*((float)rand() / RAND_MAX)) + 0.0; // from (0.0 to 1.0)
-		this->data_ML1.AmbientColor = irr::video::SColorf(red, green, blue, 0.0);
-		this->data_ML1.SpecularColor = irr::video::SColorf(red, green, blue, 0.0);
-		this->data_ML1.DiffuseColor = irr::video::SColorf(red, green, blue, 0.0);
+		// irr::f32 red = ((1.0 - 0.0)*((float)rand() / RAND_MAX)) + 0.0; // from (0.0 to 1.0)
+		// irr::f32 green = ((1.0 - 0.0)*((float)rand() / RAND_MAX)) + 0.0; // from (0.0 to 1.0)
+		// irr::f32 blue = ((1.0 - 0.0)*((float)rand() / RAND_MAX)) + 0.0; // from (0.0 to 1.0)
+		irr::f32 red = 0.8;
+		irr::f32 green = 0.1;
+		irr::f32 blue = 0.1;
+		this->data_ML1.AmbientColor = colour;//irr::video::SColorf(red, green, blue, 0.0);
+		this->data_ML1.SpecularColor = colour;//irr::video::SColorf(red, green, blue, 0.0);
+		this->data_ML1.DiffuseColor = colour;//irr::video::SColorf(red, green, blue, 0.0);
 		this->light_ML1->setLightData(data_ML1); // Apply data to the light
 	}
 
