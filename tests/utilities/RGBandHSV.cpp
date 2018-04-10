15a2ee2a2d4c76342e008dc8cdae3c792b35236e
Jamie Terry <jnterry@ntlworld.com>
2018-04-10 23:24:25 +0100

    Add tests that round trips between color spaces do not mutate data

--- tests/utilities/RGBandHSV.cpp
+++ tests/utilities/RGBandHSV.cpp
@@ -49,3 +49,36 @@ TEST_CASE("HSVtoRGB", "[client][utilities][rgbhsv]"){
 	CHECK(client::HSVtoRGB(irr::core::vector3df(360.0, 0.4, 0.9)) ==
 	      irr::video::SColor(255, 230,138,138));
 }
+
+TEST_CASE("Round trip from RGB to HSV to RGB is identity", "[client][utilities][rgbhsv]"){
+	srand(10); // make tests deterministic
+	for(int i = 0; i < 500; ++i){
+
+		irr::video::SColor pre(255,
+		                       ((float)rand() / (float)RAND_MAX) * 255,
+		                       ((float)rand() / (float)RAND_MAX) * 255,
+		                       ((float)rand() / (float)RAND_MAX) * 255
+		                       );
+
+		irr::video::SColor post = client::HSVtoRGB(client::RGBtoHSV(pre));
+
+		CHECK(pre == post);
+	}
+}
+
+TEST_CASE("Round trip from HSV to RGB to HSV is identity", "[client][utilities][rgbhsv]"){
+	srand(42); // make tests deterministic
+	for(int i = 0; i < 500; ++i){
+
+		irr::core::vector3df pre(((float)rand() / (float)RAND_MAX) * 360.0,
+		                         ((float)rand() / (float)RAND_MAX),
+		                         ((float)rand() / (float)RAND_MAX)
+		                        );
+
+		irr::core::vector3df post = client::RGBftoHSV(client::HSVtoRGBf(pre));
+
+		CHECK(pre.X == Approx(post.X).margin(0.001));
+		CHECK(pre.Y == Approx(post.Y).margin(0.001));
+		CHECK(pre.Z == Approx(post.Z).margin(0.001));
+	}
+}
