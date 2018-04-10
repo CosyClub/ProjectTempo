81c8a009a20357b5937dfe828f1316e43cdd42a1
Jamie Terry <jnterry@ntlworld.com>
2018-04-10 23:39:37 +0100

    Add unit tests for hue shift

--- tests/utilities/RGBandHSV.cpp
+++ tests/utilities/RGBandHSV.cpp
@@ -82,3 +82,63 @@ TEST_CASE("Round trip from HSV to RGB to HSV is identity", "[client][utilities][
 		CHECK(pre.Z == Approx(post.Z).margin(0.001));
 	}
 }
+
+TEST_CASE("Hue shift", "[client][utilities][rgbhsv]"){
+	CHECK(client::hueShift(irr::video::SColor(255, 255,0,0), 120.0f) ==
+	      irr::video::SColor(255, 0,255,0)
+	     );
+	CHECK(client::hueShift(irr::video::SColor(255, 0,100,0), 120.0f) ==
+	      irr::video::SColor(255, 0,0,100)
+	     );
+	CHECK(client::hueShift(irr::video::SColor(255, 0,0,50), 120.0f) ==
+	      irr::video::SColor(255, 50,0,0)
+	     );
+
+	CHECK(client::hueShift(irr::video::SColor(255, 30,0,0), -120.0f) ==
+	      irr::video::SColor(255, 0,0,30)
+	     );
+	CHECK(client::hueShift(irr::video::SColor(255, 0,0,80), -120.0f) ==
+	      irr::video::SColor(255, 0,80,0)
+	     );
+	CHECK(client::hueShift(irr::video::SColor(255, 0,200,0), -120.0f) ==
+	      irr::video::SColor(255, 200,0,0)
+	     );
+
+	CHECK(client::hueShift(irr::video::SColor(255, 50,100,150), 70.0f) ==
+	      irr::video::SColor(255, 117,50,150)
+	     );
+}
+
+TEST_CASE("Hue shift by 0 is equivalent to no-op", "[client][utilities][rgbhsv]"){
+	srand(100); // make tests deterministic
+	for(int i = 0; i < 500; ++i){
+
+		irr::video::SColorf pre((float)rand() / (float)RAND_MAX,
+		                        (float)rand() / (float)RAND_MAX,
+		                        (float)rand() / (float)RAND_MAX
+		                       );
+
+		irr::video::SColorf post = client::hueShift(pre, 0.0f);
+
+		CHECK(pre.getRed()   == Approx(post.getRed  ()).margin(0.001));
+		CHECK(pre.getGreen() == Approx(post.getGreen()).margin(0.001));
+		CHECK(pre.getBlue()  == Approx(post.getBlue ()).margin(0.001));
+	}
+}
+
+TEST_CASE("Hue shift by 360 is equivalent to no-op", "[client][utilities][rgbhsv]"){
+	srand(500); // make tests deterministic
+	for(int i = 0; i < 500; ++i){
+
+		irr::video::SColorf pre((float)rand() / (float)RAND_MAX,
+		                        (float)rand() / (float)RAND_MAX,
+		                        (float)rand() / (float)RAND_MAX
+		                       );
+
+		irr::video::SColorf post = client::hueShift(pre, 360.0f);
+
+		CHECK(pre.getRed()   == Approx(post.getRed  ()).margin(0.001));
+		CHECK(pre.getGreen() == Approx(post.getGreen()).margin(0.001));
+		CHECK(pre.getBlue()  == Approx(post.getBlue ()).margin(0.001));
+	}
+}
