2378166195f15b5811b426e22808d7bf17aa84ce
Jamie Terry <jnterry@ntlworld.com>
2018-05-07 23:30:54 +0100

    Add hacky round robin spawn slot allocation

--- src/lib-tempo/src/component/ComponentParty.cpp
+++ src/lib-tempo/src/component/ComponentParty.cpp
@@ -4,7 +4,9 @@ namespace {
 	static const constexpr int NUM_PARTIES       = 100;
 	static const constexpr int PLAYERS_PER_PARTY =   6;
 
-	static bool occupied_party_slots[NUM_PARTIES][PLAYERS_PER_PARTY] = {0};
+	//static bool occupied_party_slots[NUM_PARTIES][PLAYERS_PER_PARTY] = {0};
+
+	static int  next_party_index[NUM_PARTIES] = {0};
 }
 
 namespace tempo
@@ -19,7 +21,7 @@ namespace tempo
 	ComponentParty::ComponentParty(uint32_t party_number) :
 		party_number(party_number) {
 
-		int index = -1;
+		/*int index = -1;
 		for(int i = 0; i < PLAYERS_PER_PARTY; ++i) {
 			if(!occupied_party_slots[party_number][i]){
 				index = i;
@@ -28,7 +30,11 @@ namespace tempo
 			}
 		}
 
-		this->entity_index = index;
+		this->entity_index = index;*/
+
+		this->entity_index = next_party_index[party_number] % PLAYERS_PER_PARTY;
+	  ++next_party_index[party_number];
+
 		printf("Player has joined party %i in slot %i\n\n",
 		       this->party_number, this->entity_index);
 	}
@@ -58,7 +64,7 @@ namespace tempo
 
 	void ComponentParty::freePartySlot()
 	{
-		if(this->entity_index >= 0 && this->entity_index < PLAYERS_PER_PARTY &&
+		/*if(this->entity_index >= 0 && this->entity_index < PLAYERS_PER_PARTY &&
 		   this->party_number >= 0 && this->party_number < NUM_PARTIES
 		  ){
 			occupied_party_slots[party_number][this->entity_index] = false;
@@ -66,7 +72,7 @@ namespace tempo
 			printf("Player has left party %i in slot %i\n\n",
 		       this->party_number, this->entity_index);
 		}
-		this->entity_index = -1;
+		this->entity_index = -1;*/
 	}
 
 }  // namespace tempo
